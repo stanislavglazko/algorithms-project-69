a0d886cfed2d3e3933249191707584717bd0dee8
stanislavglazko <stanislav.glazko@dualbootpartners.com>
2023-06-12 15:30:23 +0300

    remove useless search

--- search_engine/scripts/search_engine.py
+++ search_engine/scripts/search_engine.py
@@ -2,10 +2,24 @@ import re
 from collections import defaultdict
 
 
-def search(documents: list[dict], target_word: str) -> list[str]:
+def search(documents: list[dict], target_string: str) -> list[str]:
+    target_words = target_string.split()
+    docs_with_target_words = {}
     index = generate_index_for_docs_collection(documents)
-    if target_word in index:
-        return get_reverse_sorted_by_item_list_from_dict(index[target_word])
+    for target_word in target_words:
+        if target_word not in index:
+            continue
+        docs_with_target_word = index[target_word]
+        for doc_id, word_repetition in docs_with_target_word.items():
+            if doc_id in docs_with_target_words:
+                docs_with_target_words[doc_id][0] += 1
+                docs_with_target_words[doc_id][1] += word_repetition
+            else:
+                docs_with_target_words[doc_id] = []
+                docs_with_target_words[doc_id].append(1)
+                docs_with_target_words[doc_id].append(word_repetition)
+    if docs_with_target_words:
+        return get_reverse_sorted_by_item_list_from_dict(docs_with_target_words)
     return []
 
 
@@ -36,27 +50,6 @@ def get_reverse_sorted_by_item_list_from_dict(target_dict: dict):
             target_dict.items(), key=lambda item: item[1], reverse=True
             )
         ]
-
-
-def fuzzy_search(documents: list[dict], target_string: str) -> list[str]:
-    target_words = target_string.split()
-    docs_with_target_words = {}
-    index = generate_index_for_docs_collection(documents)
-    for target_word in target_words:
-        if target_word not in index:
-            continue
-        docs_with_target_word = index[target_word]
-        for doc_id, word_repetition in docs_with_target_word.items():
-            if doc_id in docs_with_target_words:
-                docs_with_target_words[doc_id][0] += 1
-                docs_with_target_words[doc_id][1] += word_repetition
-            else:
-                docs_with_target_words[doc_id] = []
-                docs_with_target_words[doc_id].append(1)
-                docs_with_target_words[doc_id].append(word_repetition)
-    if docs_with_target_words:
-        return get_reverse_sorted_by_item_list_from_dict(docs_with_target_words)
-    return []
 
 
 if __name__ == '__main__':
